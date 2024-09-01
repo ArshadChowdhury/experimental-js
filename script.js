2095f9da924238960505fcf46136830e8def2973
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-09-01 22:28:33 +0600

    Printing star and understanding the code

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Experiment");
+
+// Star Patterns Problem
+
+function printStar(timesToPrint) {
+  let starString = "";
+  for (let i = 1; i <= timesToPrint; i++) {
+    for (let j = 0; j < i; j++) {
+      starString += "*";
+    }
+    starString += "\n";
+  }
+  return starString;
+}
+
+console.log(printStar(10));
+
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
