f05ad22ced175aae138ae02027ea3da25f3c9ffd
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-09-02 21:33:45 +0600

    Trying the other approach from the star pattern site

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ console.log("Experiment");
 
 // Star Patterns Problem
 
-function printStar(timesToPrint) {
-  let starString = "";
-  for (let i = 1; i <= timesToPrint; i++) {
-    for (let j = 0; j < i; j++) {
-      starString += "*";
-    }
-    starString += "\n";
-  }
-  return starString;
-}
-
-console.log(printStar(10));
-
-// let n = 5;
-// let string = "";
-// for (let i = 1; i <= n; i++) {
-//   for (let j = 0; j < i; j++) {
-//     string += "*";
+// function printStar(timesToPrint) {
+//   let starString = "";
+//   for (let i = 1; i <= timesToPrint; i++) {
+//     for (let j = 0; j < i; j++) {
+//       starString += "*";
+//     }
+//     starString += "\n";
 //   }
-//   string += "\n";
+//   return starString;
 // }
-// console.log(string);
+
+// console.log(printStar(10));
+
+let n = 15;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
