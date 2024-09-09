da34043f4f226f765ebfa33c6bbd9fd64648084f
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-09-10 03:13:48 +0600

    Solved series sum problem which follows a series of numbers and sums them up

--- script.js
+++ script.js
@@ -15,12 +15,41 @@ console.log("Experiment");
 
 // console.log(printStar(10));
 
-let n = 15;
-let string = "";
-for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += "*";
+// let n = 15;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// Series Sum Problem
+
+// function SeriesSum(n) {
+//   if (n == 0) {
+//     return "0.00";
+//   } else {
+//     n = n - 1;
+//     let sumArray = [1];
+//     while (n > 0) {
+//       let x = 1 / (n * 3 + 1);
+//       sumArray.push(x);
+//       n = n - 1;
+//     }
+//     return sumArray.reduce((a, b) => a + b, 0).toFixed(2);
+//   }
+// }
+
+function SeriesSum(n) {
+  let result = 0;
+
+  for (let i = 0; i < n; i++) {
+    result += 1 / (i * 3 + 1);
   }
-  string += "\n";
+
+  return result.toFixed(2);
 }
-console.log(string);
+
+console.log(SeriesSum(5));
