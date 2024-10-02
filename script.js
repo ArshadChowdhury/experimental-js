0a05f19e3c1dcc9c42b89149b53eed2bf8ab18b0
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-10-03 03:06:09 +0600

    Solved the bouncing ball problem

--- script.js
+++ script.js
@@ -42,14 +42,57 @@ console.log("Experiment");
 //   }
 // }
 
-function SeriesSum(n) {
-  let result = 0;
+// function SeriesSum(n) {
+//   let result = 0;
 
-  for (let i = 0; i < n; i++) {
-    result += 1 / (i * 3 + 1);
+//   for (let i = 0; i < n; i++) {
+//     result += 1 / (i * 3 + 1);
+//   }
+
+//   return result.toFixed(2);
+// }
+
+// console.log(SeriesSum(5));
+
+// Finding nearest Square problem
+
+// function nearestSq(n) {
+//   let nearstLow = nearst_low(n);
+//   let nearstHigh = nearst_high(n);
+
+//   return n-nearstLow < nearstHigh-n ? nearstLow : nearstHigh;
+// }
+
+// function nearst_high(n) {
+//   if (Math.sqrt(n)%1 === 0) {
+//     return n;
+//   }
+
+//   return nearst_high(++n);
+// }
+
+// function nearst_low(n) {
+//   if (Math.sqrt(n)%1 === 0) {
+//     return n;
+//   }
+
+//   return nearst_low(--n);
+// }
+
+// Bouncing ball problem
+
+function bouncingBall(h, bounce, window) {
+  if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
+    let count = 1;
+    h = h * bounce;
+    while (h > window) {
+      count += 2;
+      h = h * bounce;
+    }
+    return count;
+  } else {
+    return -1;
   }
-
-  return result.toFixed(2);
 }
 
-console.log(SeriesSum(5));
+console.log(bouncingBall(30, 0.66, 1.5));
