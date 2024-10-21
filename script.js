34b4e9ec14c26291f0b4e460471789a8f3a15209
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-10-22 02:21:37 +0600

    If the array is sorted in ascending or descending or nothing that problem solved

--- script.js
+++ script.js
@@ -179,3 +179,22 @@ console.log("Experiment");
 // const twoDecimalPlaces = (n) => Number(n.toFixed(2));
 
 // console.log(twoDecimalPlaces(4.659725356));
+
+// Sorted? yes? no? how? problem
+
+function isSortedAndHow(array) {
+  // Create copies of the array and sort them
+  let ascending = [...array].sort((a, b) => a - b);
+  let descending = [...array].sort((a, b) => b - a);
+
+  // Compare the original array with the sorted arrays
+  if (array.every((val, index) => val === ascending[index])) {
+    return "yes, ascending";
+  } else if (array.every((val, index) => val === descending[index])) {
+    return "yes, descending";
+  } else {
+    return "no";
+  }
+}
+
+console.log(isSortedAndHow([15, 7, 3, -8]));
